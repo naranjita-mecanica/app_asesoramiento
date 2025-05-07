b7adf5ad6894d235523781258c52b64583adc60f
wilmermor <wilmermor3n000@gmail.com>
2025-05-07 09:49:14 -0400

    Agregar controlador REST y método hello en BackendApplication

--- backend/src/main/java/com/smarttrainer/app/backend/BackendApplication.java
+++ backend/src/main/java/com/smarttrainer/app/backend/BackendApplication.java
@@ -2,12 +2,18 @@ package com.smarttrainer.app.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	public String hello() {
+		return "Hello World";
 	}
 
 }
