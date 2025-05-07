ece1d681412dbeb1330ecbd38246a803f0859543
Ezuchu <jesusmiguelrr17@gmail.com>
2025-05-06 21:10:37 -0400

    front base

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('Hello World!')
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</template>
