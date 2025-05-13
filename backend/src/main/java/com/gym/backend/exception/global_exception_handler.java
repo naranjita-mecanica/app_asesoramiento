af33c994ce9a57222e51c00139ddf0cba84197c5
Ezuchu <jesusmiguelrr17@gmail.com>
2025-05-13 14:03:59 -0400

    Remove unused imports in global_exception_handler.java

--- backend/src/main/java/com/gym/backend/exception/global_exception_handler.java
+++ backend/src/main/java/com/gym/backend/exception/global_exception_handler.java
@@ -1,12 +1,10 @@
 package com.gym.backend.exception;
 
-import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
--- backend/src/main/java/com/gym/backend/services/usuario_service.java
+++ backend/src/main/java/com/gym/backend/services/usuario_service.java
@@ -4,26 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gym.backend.interfaces.usuario_repo;
-import com.gym.backend.models.Usuario;
+import com.gym.backend.models.usuario;
 
 @Service
 public class usuario_service {
     @Autowired
     private usuario_repo usuarioRepo;
 
-    public Usuario guardarUsuario(Usuario usuario) {
+    public usuario guardarUsuario(usuario usuario) {
         return usuarioRepo.save(usuario); // Devuelve el usuario guardado
     }
 
-    public Usuario eliminarUsuario(int id) {
-        Usuario usuario = usuarioRepo.findById(id).orElse(null);
+    public usuario eliminarUsuario(int id) {
+        usuario usuario = usuarioRepo.findById(id).orElse(null);
         if (usuario != null) {
             usuarioRepo.deleteById(id);
         }
         return usuario;
     }
 
-    public Usuario buscarUsuario(int id) {
+    public usuario buscarUsuario(int id) {
         return usuarioRepo.findById(id).orElse(null);
     }
 }
