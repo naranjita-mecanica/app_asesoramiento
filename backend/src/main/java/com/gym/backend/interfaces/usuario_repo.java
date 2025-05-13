1ad95770b3e88de59cbd37aebf0d7af2e80d8bda
Ezuchu <jesusmiguelrr17@gmail.com>
2025-05-13 13:54:20 -0400

    Correccion usuario

--- backend/src/main/java/com/gym/backend/interfaces/usuario_repo.java
+++ backend/src/main/java/com/gym/backend/interfaces/usuario_repo.java
@@ -2,8 +2,8 @@ package com.gym.backend.interfaces;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.gym.backend.models.usuario;
+import com.gym.backend.models.Usuario;
 
-public interface usuario_repo extends JpaRepository<usuario,Integer>{
+public interface usuario_repo extends JpaRepository<Usuario,Integer>{
 
 }
--- backend/src/main/java/com/gym/backend/services/usuario_service.java
+++ backend/src/main/java/com/gym/backend/services/usuario_service.java
@@ -4,26 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gym.backend.interfaces.usuario_repo;
-import com.gym.backend.models.usuario;
+import com.gym.backend.models.Usuario;
 
 @Service
 public class usuario_service {
     @Autowired
     private usuario_repo usuarioRepo;
 
-    public usuario guardarUsuario(usuario usuario) {
+    public Usuario guardarUsuario(Usuario usuario) {
         return usuarioRepo.save(usuario); // Devuelve el usuario guardado
     }
 
-    public usuario eliminarUsuario(int id) {
-        usuario usuario = usuarioRepo.findById(id).orElse(null);
+    public Usuario eliminarUsuario(int id) {
+        Usuario usuario = usuarioRepo.findById(id).orElse(null);
         if (usuario != null) {
             usuarioRepo.deleteById(id);
         }
         return usuario;
     }
 
-    public usuario buscarUsuario(int id) {
+    public Usuario buscarUsuario(int id) {
         return usuarioRepo.findById(id).orElse(null);
     }
 }
